4f8a1a9e8a0726e677d7a06f3adf827080d48772
Katy Daviscourt and Kaz Matthews <katy+kaz@>
2017-02-15 10:08:24 -0800

    add list of options for computer to play.

--- Objects/RPS.cs
+++ Objects/RPS.cs
@@ -9,6 +9,7 @@ namespace RPSGame.Objects
     private string _player1Input;
     private string _player2Input;
     private List<string> _inputList = new List<string>();
+    private List<string> _computerPlay = new List<string> { "rock", "paper", "scissors" };
 
     public RPS (string player1Input, string player2Input)
     {
@@ -18,6 +19,7 @@ namespace RPSGame.Objects
 
     public List<string> GameMethod()
     {
+      Console.WriteLine(_computerPlay[0]);
       _inputList.Add(_player1Input);
       _inputList.Add(_player2Input);
       if(_player1Input == _player2Input)
